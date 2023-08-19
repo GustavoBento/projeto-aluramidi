8fc2249e69ca7b7c7ee5f1632c9e53a22d856d61
Gustavo Bento <133028031+GustavoBento@users.noreply.github.com>
2023-08-19 18:39:41 -0300

    finalizando o projeto

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play()
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio)
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play()
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido.')
+    }
 }
 
 const listaTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0
+for (let contador = 0; contador < listaTeclas.length; contador++) {
+    
+    const teclas = listaTeclas[contador]
+    const instrumento = teclas.classList[1]
+    const idAudio = `#som_${instrumento}`
 
-while (contador < listaTeclas.length) {
-    listaTeclas[contador] = tocaSom
-    contador = contador + 1
+    teclas.onclick = function () {
+        tocaSom(idAudio)
+    }
+
+    teclas.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space')
+
+        teclas.classList.add('ativa')
+    }
+
+    teclas.onkeyup = function () {
+        teclas.classList.remove('ativa')
+    }
 }
 
